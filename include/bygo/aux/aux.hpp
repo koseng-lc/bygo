09ff475936b6547a8c4ac222ec3beae0d209d9c9
koseng-lc <lintangerlangga@gmail.com>
2022-05-14 19:51:12 +0700

    add is_tuple and has_shape

--- include/bygo/aux/aux.hpp
+++ include/bygo/aux/aux.hpp
@@ -4,7 +4,7 @@
 namespace bygo::aux{
 
 /**
- *  @brief Using std::array 
+ *  @brief using std::array 
  */ 
 template <std::size_t _s, typename _elem_t, typename _shape_t>
 struct make_storage{
@@ -20,7 +20,7 @@ template <typename _elem_t, typename _shape_t>
 using make_storage_t = typename make_storage<_shape_t::size, _elem_t, _shape_t>::type;
 
 /**
- *  @brief Using basic array
+ *  @brief using basic array
  */ 
 template <std::size_t _s, typename _elem_t, typename _shape_t>
 struct make_storage_basic;
@@ -29,7 +29,6 @@ template <std::size_t _s, typename _elem_t, typename _shape_t>
 struct make_storage_basic{
     using array_type = typename make_storage_basic<_s-1, _elem_t, typename _shape_t::res_shape>::type;
     using type = array_type[_shape_t::dim];
-    // type data_;
 };
 
 template <typename _elem_t, typename _shape_t>
@@ -41,7 +40,7 @@ template <typename _elem_t, typename _shape_t>
 using make_storage_basic_t = typename make_storage_basic<_shape_t::size, _elem_t, _shape_t>::type;
 
 /**
- *  @brief Count the number of elements in basic_elem
+ *  @brief count the number of elements in basic_elem
  */ 
 template <std::size_t ..._ds>
 struct nelem{
@@ -57,7 +56,7 @@ template <std::size_t _d, std::size_t ..._ds>
 static constexpr auto nelem_v = nelem<_d, _ds...>::value;
 
 /**
- *  @brief Check whether the type is scalar or not
+ *  @brief check whether the type is scalar or not
  */ 
 template <typename T>
 struct is_scalar{
@@ -66,6 +65,32 @@ struct is_scalar{
 
 template <typename T>
 static constexpr auto is_scalar_v = is_scalar<T>::value;
+
+/**
+ *  @brief check whether the type is tuple or not
+ *  @tparam the type being checked
+ */ 
+template <typename T>
+struct is_tuple : std::false_type {};
+
+template <typename ...Ts>
+struct is_tuple<std::tuple<Ts...>> : std::true_type {};
+
+template <typename T>
+static constexpr auto is_tuple_v = is_tuple<T>::value;
+
+/**
+ *  @brief check whether the type has shape_type or not
+ *  @tparam the type being checked
+ */ 
+template <typename T, typename = void>
+struct has_shape : std::false_type {};
+
+template <typename T>
+struct has_shape<T, std::void_t<typename T::shape_type>> : std::true_type {};
+
+template <typename T>
+static constexpr auto has_shape_v = has_shape<T>::value;
 
 } // namespace bygo::aux
 
