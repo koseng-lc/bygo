b7d46046bb2a9559f49370709f55b7b4c4520604
koseng-lc <lintangerlangga@gmail.com>
2022-05-14 19:20:59 +0700

    reduce repeated code

--- include/bygo/op/apply.hpp
+++ include/bygo/op/apply.hpp
@@ -15,99 +15,87 @@ namespace impl{
      * @note If we use the universal reference on op_t and in_t, the deduced type include const which cause implicit conversion (?)
      * @todo reduce the boiler plate code
      */
-    template <typename shape_t, typename fn_t, typename in_t, typename op_t, typename out_t
+    template <typename shape_t, typename fn_t, typename in_t, typename op_t, typename ...ops_t, typename out_t
         , std::size_t... I, typename axes1_t, typename axes2_t, std::size_t... J, std::size_t... K, typename ...axes3_t>
-    constexpr void _apply(fn_t&& fn, in_t in, op_t op, out_t&& out
+    constexpr void _apply(fn_t&& fn, in_t in, op_t op, ops_t ...ops, out_t&& out
         , std::index_sequence<I...>, axes1_t axes1, axes2_t axes2, std::index_sequence<J...>, std::index_sequence<K...>, axes3_t... axes3){
 
         using axes1_type = util::remove_cvref_t<axes1_t>;
         using axes2_type = util::remove_cvref_t<axes2_t>;
 
+        auto dive{true};
+
         if constexpr(std::is_same<axes1_type, whole_axes_t>::value & std::is_same<axes2_type, whole_axes_t>::value){
             
             if constexpr(aux::is_scalar_v<util::remove_cvref_t<decltype(out(axes3...))>>){
                 if constexpr(aux::is_scalar_v<util::remove_cvref_t<op_t>>){
-                    out(axes3...) = fn(in(axes3...), op);
+                    out(axes3...) = fn(in(axes3...), op, ops...);
                 }else{
-                    out(axes3...) = fn(in(axes3...), op(axes3...));
+                    out(axes3...) = fn(in(axes3...), op(axes3...), ops(axes3...)...);
                 }
-            }else{
-                
-                using Is = std::make_index_sequence<shape_t::dim>;
-
-                (_apply<typename shape_t::res_shape>
-                    (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<out_t>(out)
-                    , Is{}, axes1, axes2, std::index_sequence<J...>{}, std::index_sequence<K...>{}, axes3..., I), ...);
+                dive = false;
             }
         }else if constexpr(std::is_same<axes2_type, whole_axes_t>::value){
 
             if constexpr(aux::is_scalar_v<util::remove_cvref_t<decltype(out(std::get<J>(axes1)..., axes3...))>>){
                 if constexpr(aux::is_scalar_v<util::remove_cvref_t<op_t>>){
-                    out(std::get<J>(axes1)..., axes3...) = fn(in(std::get<J>(axes1)..., axes3...), op);
+                    out(std::get<J>(axes1)..., axes3...) = fn(in(std::get<J>(axes1)..., axes3...), op, ops...);
                 }else{
-                    out(std::get<J>(axes1)..., axes3...) = fn(in(std::get<J>(axes1)..., axes3...), op(axes3...));
+                    out(std::get<J>(axes1)..., axes3...) = fn(in(std::get<J>(axes1)..., axes3...), op(axes3...), ops(axes3...)...);
                 }
-            }else{
-                
-                using Is = std::make_index_sequence<shape_t::dim>;
-
-                (_apply<typename shape_t::res_shape>
-                    (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<out_t>(out)
-                    , Is{}, axes1, axes2, std::index_sequence<J...>{}, std::index_sequence<K...>{}, axes3..., I), ...);
+                dive = false;
             }
         }else if constexpr(std::is_same<axes1_type, whole_axes_t>::value){
 
             if constexpr(aux::is_scalar_v<util::remove_cvref_t<decltype(out(axes3...))>>){
                 if constexpr(aux::is_scalar_v<util::remove_cvref_t<op_t>>){
-                    out(axes3...) = fn(in(axes3...), op);
+                    out(axes3...) = fn(in(axes3...), op, ops...);
                 }else{
-                    out(axes3...) = fn(in(axes3...), op(std::get<K>(axes2)..., axes3...));
+                    out(axes3...) = fn(in(axes3...), op(std::get<K>(axes2)..., axes3...), ops(std::get<K>(axes2)..., axes3...)...);
                 }
-            }else{
-                
-                using Is = std::make_index_sequence<shape_t::dim>;
-
-                (_apply<typename shape_t::res_shape>
-                    (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<out_t>(out)
-                    , Is{}, axes1, axes2, std::index_sequence<J...>{}, std::index_sequence<K...>{}, axes3..., I), ...);
+                dive = false;
             }
         }else{
 
             if constexpr(aux::is_scalar_v<util::remove_cvref_t<decltype(out(std::get<J>(axes1)..., axes3...))>>){
                 if constexpr(aux::is_scalar_v<util::remove_cvref_t<op_t>>){
-                    out(std::get<J>(axes1)..., axes3...) = fn(in(std::get<J>(axes1)..., axes3...), op);
+                    out(std::get<J>(axes1)..., axes3...) = fn(in(std::get<J>(axes1)..., axes3...), op, ops...);
                 }else{
-                    out(std::get<J>(axes1)..., axes3...) = fn(in(std::get<J>(axes1)..., axes3...), op(std::get<K>(axes2)..., axes3...));
+                    out(std::get<J>(axes1)..., axes3...) = fn(in(std::get<J>(axes1)..., axes3...), op(std::get<K>(axes2)..., axes3...), ops(std::get<K>(axes2)..., axes3...)...);
                 }
-            }else{
-                
-                using Is = std::make_index_sequence<shape_t::dim>;
-
-                (_apply<typename shape_t::res_shape>
-                    (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<out_t>(out)
-                    , Is{}, axes1, axes2, std::index_sequence<J...>{}, std::index_sequence<K...>{}, axes3..., I), ...);
+                dive = false;
             }
+        }
+
+        if (dive){
+            
+            using Is = std::make_index_sequence<shape_t::dim>;
+
+            (_apply<typename shape_t::res_shape>
+                (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<ops_t>(ops)..., std::forward<out_t>(out)
+                , Is{}, axes1, axes2, std::index_sequence<J...>{}, std::index_sequence<K...>{}, axes3..., I), ...);
         }
     }
 
-    template <typename shape_t, typename fn_t, typename in_t, typename op_t, typename out_t, typename axes1_t, typename axes2_t, std::size_t... I, std::size_t... J>
-    constexpr auto apply(fn_t&& fn, in_t&& in, op_t&& op, out_t&& out, axes1_t axes1, axes2_t axes2, std::index_sequence<I...>, std::index_sequence<J...>){
+    template <typename shape_t, typename fn_t, typename in_t, typename op_t, typename ...ops_t, typename out_t, typename axes1_t, typename axes2_t, std::size_t... I, std::size_t... J>
+    constexpr auto apply(fn_t&& fn, in_t&& in, op_t&& op, ops_t&& ...ops, out_t&& out, axes1_t axes1, axes2_t axes2, std::index_sequence<I...>, std::index_sequence<J...>){
 
         using Is = std::make_index_sequence<shape_t::dim>;
 
         _apply<typename shape_t::res_shape>
-            (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<out_t>(out)
+            (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<ops_t>(ops)..., std::forward<out_t>(out)
             , Is{}, std::forward<axes1_t>(axes1), std::forward<axes2_t>(axes2), std::index_sequence<I...>{}, std::index_sequence<J...>{});
     }
 } // namespace impl
 
-template <typename fn_t, typename in_t, typename op_t, typename out_t, typename axes1_t = whole_axes_t, typename axes2_t = whole_axes_t>
-constexpr auto apply(fn_t&& fn, in_t&& in, op_t&& op, out_t&& out
+template <typename fn_t, typename in_t, typename op_t, typename ...ops_t, typename out_t, typename axes1_t = whole_axes_t, typename axes2_t = whole_axes_t>
+constexpr auto apply(fn_t&& fn, in_t&& in, op_t&& op, ops_t&& ...ops, out_t&& out
                 , axes1_t&& axes1 = axes1_t{}, axes2_t&& axes2 = axes2_t{}){
 
     using out_shape = typename util::remove_cvref_t<out_t>::shape_type;
 
-    if constexpr(aux::is_scalar_v<util::remove_cvref_t<op_t>>){
+    if constexpr(aux::is_scalar_v<util::remove_cvref_t<op_t>>
+            & std::conjunction_v<aux::is_scalar_v<util::remove_cvref_t<ops_t>>...>){
 
         using axes1_type = util::remove_cvref_t<axes1_t>;
 
@@ -119,7 +107,7 @@ constexpr auto apply(fn_t&& fn, in_t&& in, op_t&& op, out_t&& out
         using Js = std::make_index_sequence<0>;
 
         impl::apply<target_out_shape>
-            (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<out_t>(out)
+            (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<ops_t>(ops)..., std::forward<out_t>(out)
             , std::forward<axes1_t>(axes1), whole_axes_t{}, Is{}, Js{});
     }else{
 
@@ -141,11 +129,11 @@ constexpr auto apply(fn_t&& fn, in_t&& in, op_t&& op, out_t&& out
 
         if constexpr(swap_axes){
             impl::apply<target_out_shape>
-                (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<out_t>(out)
+                (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<ops_t>(ops)..., std::forward<out_t>(out)
                 , std::forward<axes2_t>(axes2), std::forward<axes1_t>(axes1), Is{}, Js{});
         }else{
             impl::apply<target_out_shape>
-                (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<out_t>(out)
+                (std::forward<fn_t>(fn), std::forward<in_t>(in), std::forward<op_t>(op), std::forward<ops_t>(ops)..., std::forward<out_t>(out)
                 , std::forward<axes1_t>(axes1), std::forward<axes2_t>(axes2), Is{}, Js{});
         }
     }
